aa4bd71d2e0ce892e56bc864d570778bda331b70
Gladwinnnn <gladwin1503@hotmail.com>
2022-01-25 22:08:46 +0800

    more questions

--- Linked Lists/ReturnKthToLast.java
+++ Linked Lists/ReturnKthToLast.java
@@ -1,6 +1,8 @@
+// Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list. 
+
 import java.util.*;
 
-public class RemoveDups {
+public class ReturnKthToLast {
     public static void main(String[] args) {
         LinkedList linkedList1 = new LinkedList();
         linkedList1.append(4);
@@ -8,11 +10,11 @@ public class RemoveDups {
         linkedList1.append(4);
         linkedList1.append(2);
         linkedList1.append(4);
-        removeDuplicates(linkedList1);
+        LinkedList newLinkedList1 = returnKthToLastElement(linkedList1, 1);
 
-        Node print1 = linkedList1.head;
+        Node print1 = newLinkedList1.head;
         ArrayList<Integer> numberList1 = new ArrayList<>();
-        while (print1.next != null) {
+        while (print1 != null) {
             numberList1.add(print1.data);
             print1 = print1.next;
         }
@@ -21,30 +23,30 @@ public class RemoveDups {
         LinkedList linkedList2 = new LinkedList();
         linkedList2.append(4);
         linkedList2.append(1);
-        linkedList2.append(3);
+        linkedList2.append(4);
         linkedList2.append(2);
-        linkedList2.append(5);
-        removeDuplicates(linkedList2);
+        linkedList2.append(4);
+        LinkedList newLinkedList2 = returnKthToLastElement(linkedList2, 2);
 
-        Node print2 = linkedList2.head;
+        Node print2 = newLinkedList2.head;
         ArrayList<Integer> numberList2 = new ArrayList<>();
-        while (print2.next != null) {
+        while (print2 != null) {
             numberList2.add(print2.data);
             print2 = print2.next;
         }
         System.out.println(numberList2);
 
         LinkedList linkedList3 = new LinkedList();
+        linkedList3.append(4);
         linkedList3.append(1);
-        linkedList3.append(1);
-        linkedList3.append(1);
-        linkedList3.append(1);
-        linkedList3.append(1);
-        removeDuplicates(linkedList3);
+        linkedList3.append(4);
+        linkedList3.append(2);
+        linkedList3.append(4);
+        LinkedList newLinkedList3 = returnKthToLastElement(linkedList3, 3);
 
-        Node print3 = linkedList3.head;
+        Node print3 = newLinkedList3.head;
         ArrayList<Integer> numberList3 = new ArrayList<>();
-        while (print3.next != null) {
+        while (print3 != null) {
             numberList3.add(print3.data);
             print3 = print3.next;
         }
@@ -52,37 +54,54 @@ public class RemoveDups {
 
         LinkedList linkedList4 = new LinkedList();
         linkedList4.append(4);
-        linkedList4.append(3);
+        linkedList4.append(1);
+        linkedList4.append(4);
         linkedList4.append(2);
         linkedList4.append(4);
-        linkedList4.append(1);
-        removeDuplicates(linkedList4);
+        LinkedList newLinkedList4 = returnKthToLastElement(linkedList4, 4);
 
-        Node print4 = linkedList4.head;
+        Node print4 = newLinkedList4.head;
         ArrayList<Integer> numberList4 = new ArrayList<>();
-        while (print4.next != null) {
+        while (print4 != null) {
             numberList4.add(print4.data);
             print4 = print4.next;
         }
         System.out.println(numberList4);
+
+        LinkedList linkedList5 = new LinkedList();
+        linkedList5.append(4);
+        linkedList5.append(1);
+        linkedList5.append(4);
+        linkedList5.append(2);
+        linkedList5.append(4);
+        LinkedList newLinkedList5 = returnKthToLastElement(linkedList5, 5);
+
+        Node print5 = newLinkedList5.head;
+        ArrayList<Integer> numberList5 = new ArrayList<>();
+        while (print5 != null) {
+            numberList5.add(print5.data);
+            print5 = print5.next;
+        }
+        System.out.println(numberList5);
     }
 
-    // O(n) complexity (looping through LinkedList --> O(n), contains function in Set --> O(1))
-    public static void removeDuplicates(LinkedList linkedList) {
-        HashSet<Integer> numberList = new HashSet<>();
+    // O(n) complexity (iterating through a LinkedList --> O(n))
+    public static LinkedList returnKthToLastElement(LinkedList linkedList, int k) {
+        int counter = 1;
         Node current = linkedList.head;
+        LinkedList kthToLastElement = new LinkedList();
 
+        if (k == 1) return linkedList;
         while (current.next != null) {
-            if (numberList.contains(current.next.data)) {
-                current.next = current.next.next;
-            } else {
-                numberList.add(current.data);
+            if (k != counter) {
                 current = current.next;
+            } else if (k == counter) {
+                break;
             }
+            counter++;
         }
-        if (!numberList.contains(current.data)) {
-            linkedList.append(current.data);
-        }
+        kthToLastElement.head = current;
+        return kthToLastElement;
     }
 
     public static class Node {
@@ -102,7 +121,7 @@ public class RemoveDups {
                 head = new Node(data);
                 return;
             }
-
+            
             Node current = head;
             while (current.next != null) {
                 current = current.next;
@@ -114,24 +133,6 @@ public class RemoveDups {
             Node newNode = new Node(data);
             newNode.next = head;
             head = newNode;
-        }
-
-        public void delete(int data) {
-            if (head == null) return;
-
-            if (head.data == data) {
-                head = head.next;
-                return;
-            }
-
-            Node current = head;
-            while (current.next != null) {
-                if (current.next.data == data) {
-                    current.next = current.next.next;
-                    return;
-                }
-                current = current.next;
-            }
         }
     }
 }
